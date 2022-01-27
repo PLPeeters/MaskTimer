f51a675e298f1832b355e2999e5f944df0f66cf2
PLPeeters <PLPeeters@users.noreply.github.com>
2022-01-27 18:06:29 +0100

    Restore notifications after the app is updated

--- app/src/main/java/com/plpeeters/masktimer/SystemBroadcastReceiver.kt
+++ app/src/main/java/com/plpeeters/masktimer/SystemBroadcastReceiver.kt
@@ -10,9 +10,9 @@ import com.plpeeters.masktimer.utils.createOrUpdateMaskTimerNotification
 import com.plpeeters.masktimer.utils.setAlarmForMask
 
 
-class BootCompletedReceiver : BroadcastReceiver() {
+class SystemBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
             val notificationManager = context.getSystemService(NotificationManager::class.java)
             val alarmManager = context.getSystemService(AlarmManager::class.java)
 
